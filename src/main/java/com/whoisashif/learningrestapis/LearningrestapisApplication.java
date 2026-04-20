f0398d94f025c11131b13e4e410def134282ade9
Ashif Ali <ashif.mac@Ashifs-MacBook-Air.local>
2026-04-20 14:58:48 +0530

    Started RestAPIs project

---
+++ src/main/java/com/whoisashif/learningrestapis/LearningrestapisApplication.java
@@ -0,0 +1,13 @@
+package com.whoisashif.learningrestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningrestapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningrestapisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whoisashif/learningrestapis/LearningrestapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whoisashif.learningrestapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningrestapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
